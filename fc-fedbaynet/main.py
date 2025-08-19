80361cf253c6c39124a097b2b14f44a1c369a1f1
Prateek Anand <pingingprateek@gmail.com>
2025-08-19 08:39:18 +0200

    added structural and parametric learning functionalities

--- fc-fedbaynet/main.py
+++ fc-fedbaynet/main.py
@@ -1,4 +1,4 @@
-from bottle import Bottle
+from bottle import Bottle, run
 
 from FeatureCloud.app.api.http_ctrl import api_server
 from FeatureCloud.app.api.http_web import web_server
@@ -6,6 +6,9 @@ from FeatureCloud.app.api.http_web import web_server
 from FeatureCloud.app.engine.app import app
 
 import states
+import logging
+
+logging.getLogger("bottle").setLevel(logging.WARNING)
 
 server = Bottle()
 
@@ -14,4 +17,4 @@ if __name__ == '__main__':
     app.register()
     server.mount('/api', api_server)
     server.mount('/web', web_server)
-    server.run(host='localhost', port=5000)
+    run(server, host='localhost', port=5000, quiet=True)
